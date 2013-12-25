3ccf25c79925d3ef849295c1507fa82178bf9dc9
Rémi Audebert <contact@halfr.net>
2013-12-25 16:36:52 +0100

    server: read dummy message

--- server.go
+++ server.go
@@ -1,19 +1,48 @@
 package main
 
 import (
-	_ "bitbucket.org/evolutek/gocellaserv-protobuf"
+	"bitbucket.org/evolutek/gocellaserv-protobuf"
+	"code.google.com/p/goprotobuf/proto"
+	"encoding/binary"
+	"fmt"
 	"github.com/op/go-logging"
 	"net"
 )
+
+// Setup log
+var log = logging.MustGetLogger("cellaserv")
 
 type Service struct {
 	ip net.IPAddr
 }
 
-var log = logging.MustGetLogger("cellaserv")
+// Currently connected services
+var services []Service
 
+// Each connection is managed here
 func handle(conn net.Conn) {
-	log.Debug("New connection")
+	remoteAddr := conn.RemoteAddr()
+	log.Debug("New connection from %s", remoteAddr)
+
+	// Read message length as uint32
+	var msgLen uint32
+	err := binary.Read(conn, binary.BigEndian, &msgLen)
+	if err != nil {
+		panic(fmt.Sprint("Could not read message length:", err))
+	}
+
+	log.Debug("Message lenght: %d bytes", msgLen)
+	msgBytes := make([]byte, msgLen)
+	_, err = conn.Read(msgBytes)
+	if err != nil {
+		panic(fmt.Sprint("Could not read message:", err))
+	}
+
+	msg := &cellaserv.Message{}
+	err = proto.Unmarshal(msgBytes, msg)
+	if err != nil {
+		panic(fmt.Sprint("Could not unmarshal message:", err))
+	}
 }
 
 func main() {
