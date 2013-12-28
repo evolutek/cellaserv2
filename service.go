85d084ffd1578e21807d86d513a82d3c9516a702
Rémi (halfr) Audebert <mail@halfr.net>
2013-12-28 15:44:29 +0100

    service: add String()

--- service.go
+++ service.go
@@ -1,9 +1,18 @@
 package main
 
-import "net"
+import (
+	"fmt"
+	"net"
+)
 
 type Service struct {
 	conn           net.Conn
 	name           string
 	identification string
 }
+
+func (s *Service) String() string {
+	return fmt.Sprintf("{Service %s[%s] at %s}", s.name, s.identification, s.conn.RemoteAddr())
+}
+
+// vim: set nowrap tw=100 noet sw=8:
