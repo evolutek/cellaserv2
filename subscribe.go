51790bbaec26a2de41f404ccfcd4cb489ed28914
Rémi Audebert <halfr@halfr.net>
2014-08-20 16:29:50 +0200

    subscribe: publish log.cellaserv.new-subscriber

--- subscribe.go
+++ subscribe.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"bitbucket.org/evolutek/cellaserv2-protobuf"
+	"encoding/json"
 	"net"
 	"strings"
 )
+
+var logNewSubscriber = "log.cellaserv.new-subscriber"
+
+type LogSubscriberJSON struct {
+	Event      string
+	Subscriber string
+}
 
 func handleSubscribe(conn net.Conn, sub *cellaserv.Subscribe) {
 	log.Info("[Subscribe] %s subscribes to %s", conn.RemoteAddr(), *sub.Event)
@@ -13,6 +21,9 @@ func handleSubscribe(conn net.Conn, sub *cellaserv.Subscribe) {
 	} else {
 		subscriberMap[*sub.Event] = append(subscriberMap[*sub.Event], conn)
 	}
+
+	pub_json, _ := json.Marshal(LogSubscriberJSON{*sub.Event, connDescribe(conn)})
+	cellaservPublish(logNewSubscriber, pub_json)
 }
 
 // vim: set nowrap tw=100 noet sw=8:
